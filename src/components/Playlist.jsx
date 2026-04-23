612dd2994e51a7a948ed4f73a21ece25eb420f42
DosterToster <sevcukandrusa@gmail.com>
2026-04-23 22:13:04 +0300

    Стилізація повзунка прогресу

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,8 +1,8 @@
 export default function Playlist({ songs, setSelectedSong }) {
   return (
-    <div className="playlist">
+    <div className='playlist'>
       <h2>Playlist</h2>
-      <div className="playlist-header">
+      <div className='playlist-header'>
         <span>Title</span>
         <span>Artist</span>
         <span>Duration</span>
@@ -17,5 +17,5 @@ export default function Playlist({ songs, setSelectedSong }) {
         ))}
       </ul>
     </div>
-  )
+  );
 }
